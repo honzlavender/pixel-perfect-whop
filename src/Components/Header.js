eb82a408f4bcef9b1a67837b72967454827bd52b
Honz Williams <hnzwllms@gmail.com>
2022-03-29 11:49:29 -0400

    svg files

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import CreateIcon from "../iconComponents/createIcon";
+// import logo from './logo.png';
+
+export default function Header() {
+
+    return (
+        <header>
+            <img src="" alt="" />
+            <CreateIcon/>
+        </header>
+    )
+}
